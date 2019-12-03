6dd6975b9d9b1cb7aac22b8ce598d42450701b25
ToTo-Mo <46367491+ToTo-Mo@users.noreply.github.com>
2019-12-03 23:32:32 +0900

    자바프로그래밍 과제 : MouseEvent 오류 수정

--- src/main/java/application/task/MouseEvent/Controller.java
+++ src/main/java/application/task/MouseEvent/Controller.java
@@ -1,10 +1,12 @@
 package application.task.MouseEvent;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.Light.Point;
@@ -25,23 +27,25 @@ public class Controller implements Initializable {
         // TODO Auto-generated method stub
 
         button.setOnMouseDragged(event -> {
-            Point button_point = new Point((int) button.getLayoutY(), (int) button.getLayoutY(), 0, null);
+            Point button_point = new Point((int) button.getLayoutX(), (int) button.getLayoutY(), 0, null);
             button.setLayoutX(button_point.getX() + event.getX());
             button.setLayoutY(button_point.getY() + event.getY());
-            indicator.setText("Button Dragged : " + event.getX() + ", " + event.getY());
+            indicator.setText("Button Dragged : " + button.getLayoutX() + ", " + button.getLayoutX());
         });
 
-        if (button.getScene() != null) {
-            button.getScene().setOnMouseMoved(event -> {
+        Optional.ofNullable(button.getScene()).ifPresent(scene -> {
+            scene.setOnMouseMoved(event->{
                 indicator.setText(event.getX() + ", " + event.getY());
-            });
-        }
 
-        if (button.getScene() != null) {
-            button.getScene().setOnMouseDragged(event -> {
+            });
+        });
+
+        Optional.ofNullable(button.getScene()).ifPresent(scene -> {
+            scene.setOnMouseMoved(event->{
                 indicator.setText("dragged" + event.getX() + ", " + event.getY());
             });
-        }
+        });
+        
         click_here.setOnMouseMoved(event -> {
             indicator.setText(event.getX() + " , " + event.getY());
             event.consume();
